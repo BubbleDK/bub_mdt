05c95a3c5e1f47f2e12d5e7db55e6358f6603e42
Bubble <49866759+BubbleDK@users.noreply.github.com>
2022-11-14 23:44:08 +0100

    feat(profiles/profileExtraInformation): Added design

--- web/src/layouts/profiles/components/ProfileExtraInformation.tsx
+++ web/src/layouts/profiles/components/ProfileExtraInformation.tsx
@@ -2,7 +2,12 @@ import {
   Card, createStyles,
   Group,
   Text,
+  Grid,
+  List, ThemeIcon,
+  ScrollArea,
+  Divider
 } from '@mantine/core';
+import {IconCar, IconHomeCheck} from "@tabler/icons";
 
 const useStyles = createStyles((theme) => ({
   card: {
@@ -22,8 +27,49 @@ const ProfileExtraInformation: React.FC = () => {
           </Group>
         </Card.Section>
 
-        <Card.Section mt="sm" inheritPadding>
-          WIP
+        <Card.Section mt="sm" inheritPadding style={{maxHeight: 200}}>
+          <Grid justify="center" grow>
+            <Grid.Col span={2} style={{height: 200}}>
+              <Text weight={500}>Vehicles</Text>
+              <Divider style={{padding: 5}} />
+              <ScrollArea style={{ height: 145, padding: 5, }} type="scroll" offsetScrollbars scrollbarSize={2} scrollHideDelay={500}>
+                <List spacing="xs" size="lg" center icon={
+                    <ThemeIcon color="blue" size={24} radius="xl">
+                      <IconCar size={16} />
+                    </ThemeIcon>
+                  }
+                >
+                  <List.Item>Adder</List.Item>
+                  <List.Item>811</List.Item>
+                  <List.Item>Ardent</List.Item>
+                  <List.Item>Arbiter GT</List.Item>
+                  <List.Item>Asbo</List.Item>
+                  <List.Item>Astron</List.Item>
+                  <List.Item>Astron</List.Item>
+                </List>
+              </ScrollArea>
+            </Grid.Col>
+            <Grid.Col span={2} style={{height: 200, paddingBottom: 5}}>
+              <Text weight={500}>Properties</Text>
+              <Divider style={{padding: 5}} />
+              <ScrollArea style={{ height: 145, padding: 5, }} type="scroll" offsetScrollbars scrollbarSize={2} scrollHideDelay={500}>
+                <List spacing="xs" size="lg" center icon={
+                  <ThemeIcon color="orange" size={24} radius="xl">
+                    <IconHomeCheck size={16} />
+                  </ThemeIcon>
+                }
+                >
+                  <List.Item>Route 68 Sandy 7198</List.Item>
+                  <List.Item>South Rockford Drive</List.Item>
+                  <List.Item>Paleto Blvd</List.Item>
+                  <List.Item>Integrity Way</List.Item>
+                  <List.Item>Alta Street Plaza 4119</List.Item>
+                  <List.Item>Forum Drive</List.Item>
+                  <List.Item>Alta Street Plaza</List.Item>
+                </List>
+              </ScrollArea>
+            </Grid.Col>
+          </Grid>
         </Card.Section>
       </Card>
     </div>
